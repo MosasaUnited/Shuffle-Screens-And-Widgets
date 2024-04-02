b0882b7366c5d2e24f49033283bf21b252e3a4b6
MosasaUnited <mostafa447@hotmail.com>
2024-04-02 20:09:25 +0200

    SearchTextFieldAnimation

--- lib/feature/explore_collection_card/presentation/components/widgets/search_text_field_animation.dart
+++ lib/feature/explore_collection_card/presentation/components/widgets/search_text_field_animation.dart
@@ -14,84 +14,85 @@ class _AnimatedSearchTextFieldState extends State<AnimatedSearchTextField> {
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
-        duration: const Duration(milliseconds: 500),
-        width: opened ? MediaQuery.of(context).size.width * .9 : 56,
-        height: 56,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(32),
-            color: Colors.white,
-            boxShadow: kElevationToShadow[2]),
-        child: Row(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 16),
-                child: opened
-                    ? TextField(
-                        onSubmitted: (value) {
-                          setState(() {
-                            opened = !opened;
-                          });
-                        },
-                        style: const TextStyle(
-                          color: Colors.blue,
-                        ),
-                        decoration: const InputDecoration(
-                          hintText: 'Search',
-                          hintStyle: TextStyle(
-                            color: Colors.blue,
-                          ),
-                          border: InputBorder.none,
-                        ),
-                      )
-                    : null,
-              ),
-            ),
-            Material(
-              type: MaterialType.transparency,
+      duration: const Duration(milliseconds: 500),
+      width: opened ? MediaQuery.of(context).size.width * .9 : 56,
+      height: 56,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(32),
+          color: Colors.white,
+          boxShadow: kElevationToShadow[2]),
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16),
               child: opened
-                  ? InkWell(
-                      borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(0),
-                          topRight: Radius.circular(32),
-                          bottomLeft: Radius.circular(0),
-                          bottomRight: Radius.circular(32)),
-                      onTap: () {
+                  ? TextField(
+                      onSubmitted: (value) {
                         setState(() {
                           opened = !opened;
                         });
                       },
-                      child: const Padding(
-                        padding: EdgeInsets.all(18.0),
-                        child: Icon(
-                          Icons.close,
+                      style: const TextStyle(
+                        color: Colors.blue,
+                      ),
+                      decoration: const InputDecoration(
+                        hintText: 'Search',
+                        hintStyle: TextStyle(
                           color: Colors.blue,
-                          size: 20,
                         ),
+                        border: InputBorder.none,
                       ),
                     )
-                  : InkWell(
-                      borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(32),
-                          topRight: Radius.circular(32),
-                          bottomLeft: Radius.circular(32),
-                          bottomRight: Radius.circular(32)),
-                      onTap: () {
-                        setState(() {
-                          opened = !opened;
-                        });
-                      },
-                      child: const Padding(
-                        padding: EdgeInsets.all(18.0),
-                        child: Icon(
-                          Icons.search,
-                          color: Colors.blue,
-                          size: 20,
-                        ),
+                  : null,
+            ),
+          ),
+          Material(
+            type: MaterialType.transparency,
+            child: opened
+                ? InkWell(
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(0),
+                        topRight: Radius.circular(32),
+                        bottomLeft: Radius.circular(0),
+                        bottomRight: Radius.circular(32)),
+                    onTap: () {
+                      setState(() {
+                        opened = !opened;
+                      });
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.all(18.0),
+                      child: Icon(
+                        Icons.close,
+                        color: Colors.blue,
+                        size: 20,
                       ),
                     ),
-            ),
-          ],
-        ));
+                  )
+                : InkWell(
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(32),
+                        topRight: Radius.circular(32),
+                        bottomLeft: Radius.circular(32),
+                        bottomRight: Radius.circular(32)),
+                    onTap: () {
+                      setState(() {
+                        opened = !opened;
+                      });
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.all(18.0),
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.blue,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+          ),
+        ],
+      ),
+    );
   }
 }
